8987a6bb5324ac3575359e5e5c749406f5b95e06
corymorin11 <54879258+corymorin11@users.noreply.github.com>
2019-09-03 23:37:35 -0600

    Updated Firebase Details

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,13 @@ import "firebase/auth";
 import "firebase/firestore";
 
 const config = {
-  apiKey: "",
-  authDomain: "",
-  databaseURL: "",
-  projectId: "",
+  apiKey: "AIzaSyAJ4FBaJK-iOgIEtA8UDnGb1lvfrf4DhOU",
+  authDomain: "authentication-9126a.firebaseapp.com",
+  databaseURL: "https://authentication-9126a.firebaseio.com",
+  projectId: "authentication-9126a",
   storageBucket: "",
-  messagingSenderId: "",
-  appId: ""
+  messagingSenderId: "1009219554120",
+  appId: "1:1009219554120:web:8e3eb5eec4ae623c"
 };
 
 firebase.initializeApp(config);
